5c61a09c37e91cdfba89cc2a5a6377f3bcad8873
Reeta Singh <singh.reeta66@gmail.com>
2023-05-07 19:00:57 -0700

    adding code to create image

--- chatgpt/chatgpt-sample-client.go
+++ chatgpt/chatgpt-sample-client.go
@@ -8,7 +8,7 @@ import (
 	"github.com/sashabaranov/go-openai"
 )
 
-func main() {
+func chat() {
 	data, err := os.ReadFile("api.key")
 	if err != nil {
 		panic(err)
@@ -22,7 +22,7 @@ func main() {
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: "rephrase this bad is!",
+					Content: "rephrase in context of badminton - Smash receiving technique Your left leg is in front Your hand up , like in lift position",
 				},
 			},
 		},
@@ -34,4 +34,43 @@ func main() {
 	}
 
 	fmt.Println(resp.Choices[0].Message.Content)
+}
+
+func imageGeneration() {
+	data, err := os.ReadFile("api.key")
+	client := openai.NewClient(string(data))
+	// reqUrl := openai.ImageRequest{
+	// 	Prompt:         "Golang gopher cartoon for medium article sitting on a laptop and writing unit tests in english language",
+	// 	Size:           openai.CreateImageSize256x256,
+	// 	ResponseFormat: openai.CreateImageResponseFormatURL,
+	// 	N:              1,
+	// }
+
+	// respUrl, err := client.CreateImage(context.Background(), reqUrl)
+	// if err != nil {
+	// 	fmt.Printf("Image creation error: %v\n", err)
+	// 	return
+	// }
+	//fmt.Println(respUrl.Data[0].URL)
+	imageFile, err := os.Open("golang.png")
+	if err != nil {
+		panic(err)
+	}
+	reqUrl := openai.ImageEditRequest{
+		Image:          imageFile,
+		Prompt:         "Golang gopher cartoon dancing in rain",
+		Size:           openai.CreateImageSize256x256,
+		ResponseFormat: openai.CreateImageResponseFormatURL,
+		N:              1,
+	}
+	respUrl, err := client.CreateEditImage(context.Background(), reqUrl)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(respUrl.Data[0].URL)
+}
+
+func main() {
+	//chat()
+	imageGeneration()
 }
