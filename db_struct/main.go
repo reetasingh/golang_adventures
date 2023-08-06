28348bbfbb66fbd868ee91aec6a48756983b68a2
Reeta Singh <singh.reeta66@gmail.com>
2023-08-05 17:18:13 -0700

    basic code for creating dynamic struct

---
+++ db_struct/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type ChildStruct struct {
+	Field3 int `json:"field3,omitempty"`
+}
+
+// SourceStruct represents the source struct
+type SourceStruct struct {
+	Field1 int    `json:"field1,omitempty"`
+	Field2 string `json:"field2,omitempty"`
+	Child  ChildStruct
+	// Add other fields as needed
+}
+
+func processTag(tag reflect.StructTag) {
+	val := tag.Get("json")
+	fmt.Println(val)
+}
+
+func convertTag(tag reflect.StructTag) reflect.StructTag {
+	val := tag.Get("json")
+	s := fmt.Sprintf(`db:"%s"`, val)
+	newDBTag := reflect.StructTag(s)
+	return newDBTag
+}
+
+// CreateDynamicStruct creates a new struct type dynamically based on the fields of the source type
+func CreateDynamicStruct(sourceType reflect.Type, structName string) reflect.Type {
+	numFields := sourceType.NumField()
+	fieldTypes := make([]reflect.StructField, numFields)
+
+	for i := 0; i < numFields; i++ {
+		field := sourceType.Field(i)
+		if field.Type.Kind().String() == "struct" {
+			fieldTypes[i] = reflect.StructField{
+				Name: field.Name,
+				Type: CreateDynamicStruct(field.Type, field.Name),
+			}
+		} else {
+			fieldTypes[i] = reflect.StructField{
+				Name: field.Name,
+				Type: field.Type,
+				Tag:  convertTag(field.Tag),
+			}
+		}
+	}
+
+	newStructType := reflect.StructOf(fieldTypes)
+	return newStructType
+}
+
+func main() {
+	// Get the reflect.Type of SourceStruct
+	sourceType := reflect.TypeOf(SourceStruct{})
+
+	// Dynamically create a new struct type based on the fields of SourceStruct
+	newStructType := CreateDynamicStruct(sourceType, "DynamicStruct")
+
+	// Create an instance of the dynamically created struct type
+	newStructValue := reflect.New(newStructType).Elem()
+
+	// Print the type of the dynamically created struct
+	fmt.Printf("Type: %T\nValue: %+v\n", newStructValue.Interface(), newStructValue.Interface())
+}
